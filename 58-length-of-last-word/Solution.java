177d208c28447d05868188fc968c8ce49718ee0e
Aishwarya H <aishwaryahaishwaryah8@gmail.com>
2026-03-31 13:39:05 +0530

    Add lengthOfLastWord method in Solution class
    Implement method to calculate length of last word in a string.

---
+++ 58-length-of-last-word/Solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        String str=s.trim();
+        int last_word=0;
+        for(int i=str.length()-1;i>=0;i--)
+        {
+            if(str.charAt(i)!=' ')
+            {
+               last_word++;
+            }
+            else
+            {
+                break;
+            }
+        }
+        return last_word;        
+    }
+}
+        
