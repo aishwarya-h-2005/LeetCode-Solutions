6c6b61bf2d8e42c752c504ca9c1b2576a4e7ee04
Aishwarya H <aishwaryahaishwaryah8@gmail.com>
2026-04-06 20:51:05 +0530

    Time: 1 ms (99.65%), Space: 47.4 MB (45.42%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int jump(int[] nums) {
+        int total_jump=0;
+        int destination=nums.length-1;
+        int maximum_reach=0;
+        int last_jump_index=0;
+        if(nums.length==1) return 0;
+        for(int i=0;i<nums.length-1;i++)
+        {
+            maximum_reach=Math.max(maximum_reach,i+nums[i]);
+            if(i==last_jump_index)
+            {
+            last_jump_index=maximum_reach;
+            total_jump++;
+            }
+        }
+
+            if(maximum_reach>=destination)
+            {
+                return total_jump;
+            }
+        
+     return total_jump;
+    }
+}
