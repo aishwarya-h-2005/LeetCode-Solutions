cf396b91de7f334dd81a4d93e6cfd98dc6ce4ef9
Aishwarya H <aishwaryahaishwaryah8@gmail.com>
2026-04-08 14:07:46 +0530

    Implement reverseWords method in Solution class

---
+++ 151-length-of-last-word/Solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String reverseWords(String s) {
+        StringBuilder result=new StringBuilder();
+        String[] words=s.split(" +");
+        for(int i=words.length-1;i>=0;i--)
+        { 
+            result.append(words[i]);
+            result.append(' '); 
+        }      
+        return result.toString().trim();
+    }
+}
