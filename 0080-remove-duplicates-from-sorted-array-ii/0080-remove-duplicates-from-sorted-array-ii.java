bb2850599adb9b42244d66f3fba847664c5f989a
Aishwarya H <aishwaryahaishwaryah8@gmail.com>
2026-04-26 21:33:19 +0530

    Time: 0 ms (100%), Space: 48.7 MB (71.09%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
@@ -3,12 +3,14 @@ class Solution {
         int k=2;
         for(int i=2;i<nums.length;i++)
         {
-            if(nums[i]!=nums[k-2])
+            if(nums[i]!=nums[k-2] )
             {
                 nums[k]=nums[i];
+                
                 k++;
             }
         }
-        return k;  
-    }
+        return k;
+            
+        }
 }
